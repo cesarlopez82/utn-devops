703a2f1dfbd61b999b0cddfec5ef366cbfa7e712
CesarLopez <clopez@sumtechlabs.com>
2024-04-02 08:59:08 -0300

    new app and new scructure

---
+++ Vagrant.bootstrap.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+echo "#########################################################################"
+echo "########################## INICIANDO SCRIPT #############################"
+echo "#########################################################################"
+echo "-------------------------------------------------------------------------"
+
+#Aprovisionamiento de software
+
+#Actualizo los paquetes disponibles de la VM
+sudo apt-get update -y
+
+# Directorio para los archivos de la base de datos MySQL. El servidor de la base de datos
+# es instalado mediante una imagen de Docker. Esto está definido en el archivo
+# docker-compose.yml
+if [ ! -d "/var/db/mysql" ]; then
+	sudo mkdir -p /var/db/mysql
+fi
+
+# Muevo el archivo de configuración de firewall al lugar correspondiente
+if [ -f "/tmp/ufw" ]; then
+	sudo mv -f /tmp/ufw /etc/default/ufw
+fi
+
+##Swap
+##Genero una partición swap. Previene errores de falta de memoria
+if [ ! -f "/swapdir/swapfile" ]; then
+	sudo mkdir /swapdir
+	cd /swapdir
+	sudo dd if=/dev/zero of=/swapdir/swapfile bs=1024 count=2000000
+ 	sudo chmod 0600 /swapdir/swapfile
+	sudo mkswap -f  /swapdir/swapfile
+	sudo swapon swapfile
+	echo "/swapdir/swapfile       none    swap    sw      0       0" | sudo tee -a /etc/fstab /etc/fstab
+	sudo sysctl vm.swappiness=10
+	echo vm.swappiness = 10 | sudo tee -a /etc/sysctl.conf
+fi
+
+################################################################## CAMBIAR ESTO
+# Configuración applicación
+# ruta de la aplicación
+APP_ROOT="/opt/app"
+echo "-----------------------------> VERIFICANDO $APP_ROOT"
+if [  -d  $APP_ROOT ]; then
+    echo "-----------------------------> Creando dir $APP_ROOT"
+	sudo mkdir -p $APP_ROOT
+fi
+
+# ruta de la aplicación
+APP_PATH="$APP_ROOT/utn-devops-app"
+# Verificar si el repositorio ya ha sido clonado
+echo "-----------------------------> VERIFICANDO $APP_PATH/.git"
+if [ -d "$APP_PATH/.git" ]; then
+    echo "-----------------------------> ACTUALIZANDO!"
+    echo "El repositorio ya ha sido clonado. Actualizando..."
+    cd $APP_PATH
+    sudo git reset --hard HEAD
+    sudo git pull origin main
+else
+    # Si el directorio .git no existe, clonar el repositorio
+    echo "-----------------------------> CLONANDO!"
+    echo "Clonando el repositorio..."
+    sudo git clone https://github.com/cesarlopez82/utn-devops-app.git $APP_PATH
+fi
+
+cd $APP_PATH
+
+##################################################################
+
+######## Instalacion de DOCKER ########
+#
+# Esta instalación de docker es para demostrar el aprovisionamiento
+# complejo mediante Vagrant. La herramienta Vagrant por si misma permite
+# un aprovisionamiento de container mediante el archivo Vagrantfile. A fines
+# del ejemplo que se desea mostrar en esta unidad que es la instalación mediante paquetes del
+# software Docker este ejemplo es suficiente, para un uso más avanzado de Vagrant
+# se puede consultar la documentación oficial en https://www.vagrantup.com
+#
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+if [ ! -x "$(command -v docker)" ]; then
+	sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg
+
+	##Configuramos el repositorio
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+	sudo chmod a+r /usr/share/keyrings/docker-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	
+	#Actualizo los paquetes con los nuevos repositorios
+	sudo apt-cache policy docker-ce
+	sudo apt-get update -y
+	#Instalo docker desde el repositorio oficial
+	sudo apt-get -y  install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+	
+	#Lo configuro para que inicie en el arranque
+	sudo systemctl enable docker
+fi
+
+# ruta al directorio docker
+DOCKER_PATH="$APP_PATH/docker"
+echo "-----------------------------> VERIFICANDO $DOCKER_PATH"
+if [ -d "$DOCKER_PATH" ]; then
+    echo "-----------------------------> VERIFICANDO docker-compose.yml"
+    if [ -f "$DOCKER_PATH/docker-compose.yml" ]; then
+        echo "El archivo docker-compose.yml existe."
+        cp $APP_PATH/app.py $DOCKER_PATH
+        cd $DOCKER_PATH
+        echo "-----------------------------> DESTRUYENDO CONTENEDORES"
+        sudo docker-compose down
+        # Verificar que docker-compose down haya terminado correctamente
+        if [ $? -eq 0 ]; then
+            echo "docker-compose down ha terminado correctamente. Iniciando docker-compose up -d --build..."
+
+            # Recreando contenedores.
+            echo "-----------------------------> RECREANDO CONTENEDORES"
+            docker-compose up -d --build
+        else
+            echo "ERROR: docker-compose down -v ha fallado. No se puede continuar."
+        fi        
+    else
+        echo "El archivo docker-compose.yml no existe."
+    fi
+fi
+
+
+
+
--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,7 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
+
 Vagrant.configure("2") do |config|
   
   if Vagrant.has_plugin?("vagrant-vbguest") then
@@ -9,18 +10,12 @@ Vagrant.configure("2") do |config|
   
   # Imagen por defecto
   box = 'ubuntu/jammy64'
-  
-  # Si se ejecuta sobre macOS se configura otra imagen
+
+  #Si se ejecuta sobre macOS se configura otra imagen
   if Vagrant::Util::Platform.darwin? 
     box = "bento/ubuntu-22.04-arm64"
   else
-    config.vm.provision "shell", inline: "sudo apt-get update && sudo apt-get install -y virtualbox-guest-x11 python3-pip"
-    config.vm.provision "shell", inline: <<-SHELL
-      sudo pip3 install flask
-      cd /vagrant
-      sudo chmod +x /vagrant/app.py
-      sudo python3 app.py
-    SHELL
+    config.vm.provision "shell", inline: "sudo apt-get update && sudo apt-get install -y virtualbox-guest-x11"
   end
 
   # Con esto le indicamos a Vagrant que vaya al directorio de "cajas" (boxes) que contiene su Atlas e instale un
@@ -29,12 +24,13 @@ Vagrant.configure("2") do |config|
   config.vm.box = box
 
   # Redirecciono puertos desde la maquina virtual a la maquina real. Por ejemplo
-  # del puerto 80 (web) de la maquina virtual con Debian se podrá acceder a través
-  # del puerto 8080 de nuestro navegador.
+  # del puerto 8081 (web) de la maquina virtual se podrá acceder a través
+  # del puerto 8081 de nuestro navegador.
   # Esto se realiza para poder darle visibilidad a los puertos de la maquina virtual
   # y además para que no se solapen los puertos con los de nuestra equipo en el caso de que
   # ese número de puerto este en uso.
-  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 8081, host: 8081, auto_correct: true
+  config.vm.network "forwarded_port", guest: 4400, host: 4400, auto_correct: true
 
   # Permite descargas con certificados vencidos o por http
   config.vm.box_download_insecure = true
@@ -56,5 +52,13 @@ Vagrant.configure("2") do |config|
   # Configuro la cantidad de memoria ram de la VM para el proveedor VMware
   config.vm.provider "vmware_desktop" do |vm|
     vm.memory = "1024"
+
+  # Este comando transfiere un archivo desde la maquina host a la maquina cliente. 
+  # Es para permitir el redireccionamiento de tráfico entre el máquina host y la vm
+  config.vm.provision "file", source: "hostConfigs/ufw", destination: "/tmp/ufw"
+
+  # En este archivo tendremos el provisionamiento de software necesario para nuestra
+  # maquina virtual. Por ejemplo, servidor web, servidor de base de datos, etc.
+  config.vm.provision :shell, path: "Vagrant.bootstrap.sh", run: "once"
   end
 end
